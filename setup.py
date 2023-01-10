1bc3e7df8ae7884179ea01abda9eb569ec6d2e4f
Michel Doré <michel@doremail.nl>
2023-01-10 15:44:12 +0100

    Update long descr

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(
     author="Michel Doré",
     author_email="",
     description="A package to download Dutch parliament debates and subtitles",
+    long_description="Using this package you can download Dutch parliament debates and subtitles. It is based on the website https://debatgemist.tweedekamer.nl. This package is not affiliated with the Dutch parliament. It is not allowed to use this package for commercial purposes.",
     package_dir={'': 'tweedekamer'},
     packages=setuptools.find_packages(where='tweedekamer'),
 )
